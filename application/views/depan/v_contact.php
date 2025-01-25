fe915ab2b5f1b78fcb9901b7f96545e4fe197612
Wahyu Sofianda <wahyusofianda9d@gmail.com>
2025-01-25 22:45:04 +0700

    first commit

---
+++ application/views/depan/v_contact.php
@@ -0,0 +1,50 @@
+<style>
+    .map-frame {
+        position: relative;
+        overflow: hidden;
+        padding: 10px;
+        border: 2px solid #000;
+        border-radius: 10px;
+        height: 100%; /* Set tinggi menjadi 100% */
+    }
+
+    .map-frame iframe {
+        width: 100%;
+        height: 100%; /* Set tinggi iframe menjadi 100% */
+        position: absolute;
+        top: 0;
+        left: 0;
+        border: none;
+    }
+
+    .info {
+        padding: 20px;
+        border: 2px solid #000;
+        border-radius: 10px;
+        height: 100%; /* Set tinggi menjadi 100% */
+    }
+
+</style>
+
+<section class="contact-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="map-frame">
+                    <iframe src="https://www.google.com/maps/place/MTs+Swasta+Darul+Ulum+Budi+Agung/@3.6892058,98.6554201,17z/data=!3m1!4b1!4m6!3m5!1s0x30312d2014a6c087:0x52e88e3df91c9c2!8m2!3d3.6892005!4d98.657995!16s%2Fg%2F11s3zkv8zm?entry=ttu&g_ep=EgoyMDI1MDEyMi4wIKXMDSoASAFQAw%3D%3D" width="100%" height="100%" frameborder="0" allowfullscreen="allowfullscreen" aria-hidden="false" tabindex="0" style="border: 0;"></iframe>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="info">
+                    <h3>Informasi Kontak</h3>
+                    <hr>
+                    <p>
+                        <i aria-hidden="true" class="fa fa-map-marker"></i>  Kp. Kali Ulu RT.02/01, Karangraharja, Kec. Cikarang Utara, Kab. Bekasi Prov. Jawa Barat<br><br>
+                        <i class="fas ti-tablet"></i> 089623772786 <br><br>
+                        <i class="fas ti-email"></i> alhudriwalibrah2001@gmail.com
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ application/views/theme/adminfooter.php
@@ -0,0 +1,6 @@
+<div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Copyright &copy; Sistem Informasi Sekolah SMPI AL-HUDRI WALIBRAH | Repost by <a href='https://stokcoding.com/' title='StokCoding.com' target='_blank'>StokCoding.com</a>
+            </span>
+          </div>
+        </div>
